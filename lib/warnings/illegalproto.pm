cddeca21f3ee71cb39a15c7737860d87285231e8
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-11-11 21:52:13 -0600

    add docs

--- lib/warnings/illegalproto.pm
+++ lib/warnings/illegalproto.pm
@@ -31,3 +31,24 @@ sub unimport {
 }
 
 1;
+
+=pod
+
+=HEAD1 SYNOPSIS
+
+ use strictures 1;
+ use signatures;
+ no warnings::illegalproto;
+
+=HEAD1 DESCRIPTION
+
+This module was implemented so that people can C<< use strictures >> and
+C<< use signatures >> at the same time.  Thanks to mst, in Perl 5.12 and
+greater this is trivial, but before that a strange dance had to be done.
+
+This module will do the right thing for both before and after 5.12, but if you
+want to use the native 5.12 and greater without this module, feel free to cargo
+cult the following:
+
+ no if $[ >= 5.012, warnings => 'illegalproto';
+ no if $[ < 5.012, 'warnings::illegalproto';
